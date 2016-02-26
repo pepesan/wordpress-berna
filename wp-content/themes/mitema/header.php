1ecae1a41b1abbbf96191a23798e9d2521c436db
David Vaquero Santiago <pepesan@gmail.com>
2016-02-26 09:34:44 +0100

    primera subida del tema

---
+++ wp-content/themes/mitema/header.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css"  />
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
+<body>
+<div class="header">
+<p>This is header section. Put your logo and other details here.</p>
+</div>
